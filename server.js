2ec3f316fb10e7e0faf479bbded6808cc5af28a3
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-11 14:31:56 +0200

    connected to mongoose atlas

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-
+mongoose
+  .connect(
+    "mongodb+srv://hubermanophir:Password123@testcluster.oqqu5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected Successfully to Mongoose atlas"));
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.post("/api/exercise/new-user");
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
